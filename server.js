07c6a7a265244572be6273e990330c7f33476afc
xiyu-526 <997578871@qq.com>
2020-05-07 12:24:17 +0800

    初始化

---
+++ server.js
@@ -0,0 +1,22 @@
+let app  = require("http");
+
+let server = app.createServer(function(req,res){
+    //设置允许跨域的域名，*代表允许任意域名跨域
+    res.setHeader("Access-Control-Allow-Origin","*");
+
+    // 发送响应数据 "Hello World"
+    if(req.url === "/api"){
+        res.end(JSON.stringify([
+            {id:1, name: "zt"},
+            {id:2, name: "zt2"},
+            {id:3, name: "zt3"}
+        ]));
+    }else{
+        res.end('Not Found 200');
+    }
+    
+})
+
+server.listen(3000,function(){
+    console.log("服务器已经连接！");
+})
